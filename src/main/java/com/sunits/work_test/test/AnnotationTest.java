b78730d70d3d2fdd55a015b9823a7018a68b659b
xieyunsheng <m17611476835@163.com>
2022-01-02 23:38:47 +0800

    111

--- src/main/java/com/sunits/work_test/test/AnnotationTest.java
+++ src/main/java/com/sunits/work_test/test/AnnotationTest.java
@@ -2,13 +2,11 @@ package com.sunits.work_test.test;
 
 import com.sunits.work_test.annotation.Info;
 import com.sunits.work_test.entity.Person;
-import org.apache.commons.lang3.StringUtils;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.File;
 
 public class AnnotationTest {
-    public static void main2(String[] args) {
+    public static void main1(String[] args) {
         try {
             //获取Person的Class对象
             Person person = Person.builder().build();
@@ -27,9 +25,51 @@ public class AnnotationTest {
         }
     }
 
+    /**
+     * 通过文件路径直接修改文件名
+     *
+     * @param filePath    需要修改的文件的完整路径
+     * @param newFileName 需要修改的文件的名称
+     * @return
+     */
+    private static String FixFileName(String filePath, String newFileName) {
+        File f = new File(filePath);
+        if (!f.exists()) { // 判断原文件是否存在（防止文件名冲突）
+            return null;
+        }
+        newFileName = newFileName.trim();
+        if ("".equals(newFileName) || newFileName == null) // 文件名不能为空
+            return null;
+        String newFilePath = null;
+        if (f.isDirectory()) { // 判断是否为文件夹
+            newFilePath = filePath.substring(0, filePath.lastIndexOf("/")) + "/" + newFileName;
+        } else {
+            newFilePath = filePath.substring(0, filePath.lastIndexOf("/")) + "/" + newFileName;
+        }
+        File nf = new File(newFilePath);
+        try {
+            f.renameTo(nf); // 修改文件名
+        } catch (Exception err) {
+            err.printStackTrace();
+            return null;
+        }
+        return newFilePath;
+    }
+
     public static void main(String[] args) {
-        Integer aaa= null;
-        System.out.println(aaa);
-        System.out.println(LocalDateTime.parse("2021-12-08 06:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:ss:mm")));
+        /*String filePath = "F:\\2021QGLOUFENG-R-TW\\2021年全国楼凤信息合集（附带联系方式）R\\安徽省";
+        File file = new File(filePath);
+        File[] fileArray= file.listFiles();
+        File fileName = null;
+        for(int i =0;i<fileArray.length;i++){
+            fileName = fileArray[i];
+            System.out.println(fileName.getName().substring(0,3)+".rar");;
+            FixFileName(filePath+"/"+fileName.getName(),fileName.getName().substring(0,3)+".rar");
+        }*/
+        if (1==2){
+            System.out.println("52552");
+        }{
+            System.out.println("aaa");
+        }
     }
 }
